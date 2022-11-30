b441652269d81ff9819fe649540f35a3a6568a1c
udeaghad <dozie.udeagha@gmail.com>
2022-11-30 02:17:52 +0100

    update auth cookies

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -36,7 +36,7 @@ export const login = async(req, res, next) => {
      const {password, ...others} = user._doc;
 
      res
-     .cookie("access_token", token, { httpOnly: true})
+     .cookie("access_token", token, { httpOnly: true, sameSite: "none" })
      .status(200).json({user:others, token: token})
      
   } catch (error) {
